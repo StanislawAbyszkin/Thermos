a42e037f384a6951c66ddec059e297244beaf34a
Stanislaw Abyszkin <Stasiek@Stanislaws-MacBook-Pro.local>
2016-11-10 21:04:23 +0000

    added flash messages to display what was given by the user

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect, flash
 
+from forms import BookmarkForm
+
 app = Flask(__name__)
-app.logger.setLevel(DEBUG)
+app.config['SECRET_KEY'] = '\xfe\xd0>\xf2\x9ds\x1b\xa6N#;\xfd\xec#\x8a\xa7\xd3g\x93\t~\xe8l\xb1'
 
 bookmarks = []
 
@@ -11,10 +13,19 @@ def store_bookmark(url):
                           user= "Stasiu",
                           date= datetime.utcnow()))
 
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Moj tytul z kodu", text="To dziala calkiem fajnie jak na razie.")
+    return render_template('index.html',
+                           title="Moj tytul z kodu",
+                           text="To dziala calkiem fajnie jak na razie.",
+                           new_bookmarks=new_bookmarks(5)
+                           )
 
 @app.route("/add", methods=['GET','POST'])
 def add():
@@ -22,6 +33,7 @@ def add():
         url = request.form['url']
         store_bookmark(url)
         app.logger.debug('stored url ' + url)
+        flash("Stored bookmark {0}".format(url))
         return redirect(url_for('index'))
     return render_template('add.html')
 
@@ -34,4 +46,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
