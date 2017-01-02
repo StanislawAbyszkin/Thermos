ce86df5c11ba28c5fc825c5ec869ff77442b0b6b
Stanislaw Abyszkin <Stasiek@Stanislaws-MacBook-Pro.local>
2017-01-02 14:13:07 +0100

    added missing files...

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -28,5 +28,5 @@ moment = Moment(app)
 import thermos.views
 import thermos.models
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
